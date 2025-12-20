36ed1ef49a552eb8a99cc4abbcda0bfee78f5689
Daniel Smolsky <dannysmo@gmail.com>
2025-12-20 12:30:13 -0500

    refactor: rename prune-tool.ts to tools.ts to better reflect its contents

--- lib/strategies/index.ts
+++ lib/strategies/index.ts
@@ -1,4 +1,4 @@
 export { deduplicate } from "./deduplication"
 export { runOnIdle } from "./on-idle"
-export { createDiscardTool, createExtractTool } from "./prune-tool"
+export { createDiscardTool, createExtractTool } from "./tools"
 export { supersedeWrites } from "./supersede-writes"
